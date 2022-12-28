0ca402a063da555151e729670add03fcc1573247
Saeed Ahmed <saeedchachar987654@gmail.com>
2022-12-28 15:57:16 +0500

    last

--- server.js
+++ server.js
@@ -40,14 +40,14 @@ mongoose.connect(config.mongoURI, {
     useCreateIndex: true
 }).then(() => console.log('MongoDb Connected')).catch(err => console.log(err));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('./client/build'));
 
-    app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+//     app.get('/*', (req, res) => {
+//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 
-    });
-}
+//     });
+// }
 
 app.listen(process.env.PORT || 8000, () => console.log('Listening to port 8000'));
 
